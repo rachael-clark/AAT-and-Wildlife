b6f4fce26d27075c2fa8445dddaafd3850df2b24
rachael-clark <2235694c@student.gla.ac.uk>
2021-03-05 18:57:50 +0000

    Addition of code for sensitivity testing

--- sample_model_comparison_plots.R
+++ sample_model_comparison_plots.R
@@ -83,3 +83,73 @@ write.csv(outpop70,"~/University/4th Year/AAT-and-Wildlife/outputs/Population70.
 write.csv(outpop80,"~/University/4th Year/AAT-and-Wildlife/outputs/Population80.csv")
 write.csv(outpop90,"~/University/4th Year/AAT-and-Wildlife/outputs/Population90.csv")
 write.csv(outpop100,"~/University/4th Year/AAT-and-Wildlife/outputs/Population100.csv")
+
+#Sensitivity Testing of Tsetse Species at Three Different Wildlife Proportions
+par(mfrow=c(1,3))
+plot(out4.gpall10$Inf1 ~ seq(1,1001,1),type='l',col='darkgreen',lwd=3,ylim=c(0,300),
+     ylab="Infected Cattle",xlab="Time",main="10% Wildlife")
+lines(out4.gswyn10$Inf1 ~ seq(1,1001,1), lwd=3, col='blue')
+legend('topright', legend=c("G. pallidipes","G. swynnertoni"),
+       col=c("darkgreen", "blue"), lty=1, cex=0.8, lwd=3)
+
+plot(out4.gpall50$Inf1 ~ seq(1,1001,1),type='l', col='darkgreen',lwd=3,ylim=c(0,300),#
+     ylab="Infected Cattle",xlab="Time",main="50% Wildlife")
+lines(out4.gswyn50$Inf1 ~ seq(1,1001,1),lwd=3, col='blue')
+legend('topright', legend=c("G. pallidipes","G. swynnertoni"),
+       col=c("darkgreen", "blue"), lty=1, cex=0.8, lwd=3)
+
+plot(out4.gpall90$Inf1 ~ seq(1,1001,1),type='l', col='darkgreen',lwd=3,ylim=c(0,300),#
+     ylab="Infected Cattle",xlab="Time",main="90% Wildlife")
+lines(out4.gswyn90$Inf1 ~ seq(1,1001,1),lwd=3, col='blue')
+legend('topright', legend=c("G. pallidipes","G. swynnertoni"),
+       col=c("darkgreen", "blue"), lty=1, cex=0.8, lwd=3)
+
+#Comparison of the Tsetse Species: 
+
+par(mfrow=c(1,3))
+plot(out4.gpall50$Inf1 - out4.gswyn50$Inf1 ~ seq(1,1001,1), ylim = c(-60,60), type = 'l', lwd=3,
+     ylab = "Difference between Tsetse Species", xlab = "Time", col = 'red',main="50% Wildlife Proportion")
+abline(h=0, lty = 2, lwd = 2)
+
+plot(out4.gpall10$Inf1 - out4.gswyn10$Inf1 ~ seq(1,1001,1), ylim = c(-60,60), type = 'l', lwd=3,
+     ylab = "Difference between Tsetse Species", xlab = "Time", col = 'red',main="10% Wildlife Proportion")
+abline(h=0, lty = 2, lwd = 2)
+
+plot(out4.gpall90$Inf1 - out4.gswyn90$Inf1 ~ seq(1,1001,1), ylim = c(-60,60), type = 'l', lwd=3,
+     ylab = "Difference between Tsetse Species", xlab = "Time", col = 'red',main="90% Wildlife Proportion")
+abline(h=0, lty = 2, lwd = 2)
+
+write.csv(out4.gpall10,"~/University/4th Year/AAT-and-Wildlife/outputs/Gpall10.csv")
+write.csv(out4.gpall50,"~/University/4th Year/AAT-and-Wildlife/outputs/Gpall50.csv")
+write.csv(out4.gpall90,"~/University/4th Year/AAT-and-Wildlife/outputs/Gpall90.csv")
+write.csv(out4.gswyn10,"~/University/4th Year/AAT-and-Wildlife/outputs/Gswyn10.csv")
+write.csv(out4.gswyn50,"~/University/4th Year/AAT-and-Wildlife/outputs/Gswyn50.csv")
+write.csv(out4.gswyn90,"~/University/4th Year/AAT-and-Wildlife/outputs/Gswyn90.csv")
+
+#Sensitivity Testing of Wildlife Increases at Differnet Wildlife Proportions 
+par(mfrow=c(1,3))
+plot(outpop0$Inf1 ~ seq(1,1001,1),type='l',col='darkgreen',lwd=3,ylim=c(0,300),
+     ylab="Infected Cattle",xlab="Time")
+lines(outpop10$Inf1 ~ seq(1,1001,1), lwd=3, col='blue')
+lines(outpop20$Inf1~ seq(1,1001,1), lwd=3, col='red')
+lines(outpop30$Inf1~ seq(1,1001,1), lwd=3, col='purple')
+lines(outpop30$Inf1~ seq(1,1001,1), lwd=3, col='orange')
+lines(outpop40$Inf1~ seq(1,1001,1), lwd=3, col='pink')
+lines(outpop50$Inf1~ seq(1,1001,1), lwd=3, col='brown')
+lines(outpop60$Inf1~ seq(1,1001,1), lwd=3, col='yellow')
+lines(outpop70$Inf1~ seq(1,1001,1), lwd=3, col='lightgreen')
+lines(outpop80$Inf1~ seq(1,1001,1), lwd=3, col='lightblue')
+lines(outpop90$Inf1~ seq(1,1001,1), lwd=3, col='darkblue')
+lines(outpop100$Inf1~ seq(1,1001,1), lwd=3, col='black')
+legend('topright', legend = c("0% Increase", "10% Increase","20% Increase" , "30% Increase", "40% Increase",
+                              "50% Increase","60% Increase", "70% Increase","80% Increase",
+                              "90% Increase","100% Increase"),
+       col=c("darkgreen","blue","red","purple","orange","pink",
+             "brown", "yellow","lightgreen","lightblue","darkblue","black"),
+       lty=1, cex=0.8, lwd=3)
+
+
+#Code for Altering the Rplots 
+axis(1)
+axis(2, at = c(0,50,100,150,200))
+Axes = FALSE
